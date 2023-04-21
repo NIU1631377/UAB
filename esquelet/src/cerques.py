879b52084787e43ae7d5f70ddd1b274bb039e0d7
NIU1631377 <93310914+NIU1631377@users.noreply.github.com>
2023-04-21 15:35:52 +0200

    Add files via upload

---
+++ esquelet/src/cerques.py
@@ -0,0 +1,28 @@
+##########################################################################
+#########               Funcions del seminari 3 de MD           ##########
+#########              Copyright 2022 Bernat Gastón             ##########
+#########               Author: Bernat Gastón                   ##########
+#########            Contact: bernat.gaston@uab.cat             ##########
+#########   Alumnes:                                            ##########
+##########################################################################
+
+from collections import deque
+
+'''
+Donat un node inicial en un graf, descobreix el graf mitjançant l'algorisme BFS
+@param: Node inicial
+@return: llista de nodes visitats
+'''
+def BFS(init_node):
+    return list()
+
+'''
+Donat un node inicial en un graf, descobreix el graf mitjançant l'algorisme DFS
+@param: Node inicial
+@return: llista de nodes visitats
+'''
+def DFS(init_node):
+    return list()
+
+
+        
---
+++ solucio/src/main.py
@@ -0,0 +1,21 @@
+##########################################################################
+#########            Funció main del seminari 3 de MD           ##########
+#########              Copyright 2022 Bernat Gastón             ##########
+#########               Author: Bernat Gastón                   ##########
+#########            Contact: bernat.gaston@uab.cat             ##########
+#########   Alumnes:                                            ##########
+##########################################################################
+
+from graph import Node, Graph
+from cerques import BFS, DFS
+
+'''
+Funció que crea un graf simulant una mini internet
+@return: el graf creat
+'''
+def crea_internet():
+    return None
+
+
+if __name__ == '__main__':
+    internet = crea_internet()
